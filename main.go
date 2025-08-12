2e17e1b38a493d3c18b5b7a79719d8c45cc3dc39
Elliott Cepin <elliottcepin5@gmail.com>
2025-08-12 15:07:14 +0000

    Basic template working + file structure added

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"html/template"
 )
+
+var tmpl = template.Must(template.ParseFiles("templates/html/home.html"))
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, friend!")
-	})
+		data := map[string]string{
+			"Title": "Home Page",
+			"Message": "Hello, friend!",
+		}
 
-	http.HandleFunc("/testing-tags", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "<h1>This page uses tags</h1>")
+		if err := tmpl.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
-
+	
 	log.Println("Server running at http://elliottcepin.dev:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
