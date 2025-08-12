596c689a2bc4ac08bebbd6b2b7092fefe81158f6
Elliott Cepin <elliottcepin5@gmail.com>
2025-08-12 14:49:39 +0000

    testing tags at /testing-tags

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ func main() {
 		fmt.Fprintln(w, "Hello, friend!")
 	})
 
+	http.HandleFunc("/testing-tags", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "<h1>This page uses tags</h1>")
+	})
+
 	log.Println("Server running at http://elliottcepin.dev:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
