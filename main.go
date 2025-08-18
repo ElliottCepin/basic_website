b10fc883f7bccfdff9b26549a92944d1918d018f
Elliott Cepin <elliottcepin5@gmail.com>
2025-08-18 17:22:45 +0000

    got js to render in canvas

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 var tmpl = template.Must(template.ParseFiles("templates/home.html"))
-
+var tic_tac_toe = template.Must(template.ParseFiles("templates/tictactoe.html"))
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := map[string]string{
@@ -19,12 +19,21 @@ func main() {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
+	http.HandleFunc("/tic-tac-toe", func(w http.ResponseWriter, r *http.Request) {
+		data := map[string]string {
+			"Title": "Tic Tac Toe",
+		}
+
+		if err := tic_tac_toe.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		un := r.FormValue("username")
 		fmt.Println(un)
-	});
+	})
 	
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	log.Println("Server running at http://elliottcepin.dev:8080")
