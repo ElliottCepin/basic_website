5d2a572a3864562c5fd6c64456f3c851f53fc383
Elliott Cepin <elliottcepin5@gmail.com>
2025-08-18 21:11:41 +0000

    tic tac toe works as well as on a napkin

--- static/tictactoe.js
+++ static/tictactoe.js
@@ -1,11 +1,15 @@
 const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
-
+var counter = 0;
+var game_state = {
+	board: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+}
 
 function drawBoard(){
-	x = [100, 100, 200, 200, 0, 310, 0, 310];
-	y = [0, 310, 0, 310, 100, 100, 200, 200];
-
+	ctx.clearRect(0, 0, canvas.width, canvas.height);	
+	var x = [100, 100, 200, 200, 0, 310, 0, 310];
+	var y = [0, 310, 0, 310, 100, 100, 200, 200];
+	var tileX, tileY;
 	for (var i = 0; i < x.length; i++){
 		ctx.beginPath();
 		ctx.moveTo(x[i],y[i]);
@@ -13,6 +17,22 @@ function drawBoard(){
 		ctx.lineTo(x[i], y[i]);
 		ctx.stroke();
 	}
+
+	for (var j=0; j < game_state.board.length; j++){
+		
+		[tileX, tileY] = getRegionCenter(j);
+		
+		if (game_state.board[j] == 0) {
+			continue;
+		}
+
+		if (game_state.board[j] == 1) {
+			drawX(tileX, tileY);
+		} else if (game_state.board[j] == 2) {
+			drawO(tileX, tileY);
+		}
+	}
+
 }
 
 function drawX(x,y){
@@ -25,16 +45,32 @@ function drawX(x,y){
 	ctx.stroke();
 }
 
-drawBoard();
+function drawO(x,y) {
+	ctx.beginPath();
+	ctx.arc(x, y, 25, 0, 2*Math.PI);
+	ctx.stroke()
+}
+
 
 canvas.addEventListener('click', function(event) {
 	var clickX = event.pageX - canvas.offsetLeft;
 	var clickY = event.pageY - canvas.offsetTop;
 	var region = getRegion(clickX, clickY)
 	var [tileX, tileY] = getRegionCenter(region);
-	drawX(tileX,tileY);
+	if (counter % 2 == 0) {
+		if (game_state.board[region] == 0) {
+			game_state.board[region] = 1
+			counter++;
+		}
+	} else {
+		if (game_state.board[region] == 0) {
+			game_state.board[region] = 2
+			counter++;
+		}
+	}
+	drawBoard();
 
-})
+});
 
 function getRegion(x, y) {
 	var r = Math.floor(x/100);
@@ -48,3 +84,5 @@ function getRegionCenter(r){
 
 	return [x, y];
 }
+
+drawBoard();
